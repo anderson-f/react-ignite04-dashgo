74a196509f4653fe57ae0e31dc1b7d76794cbe3c
Anderson Freitas <andersonfreitas.tec@gmail.com>
2021-10-02 12:05:01 -0300

    feat: start application nav

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -12,6 +12,7 @@ import React from "react";
 import { Input } from "../../components/Form/Input";
 import Header from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import Link from "next/link";
 
 export default function CreateUser() {
   return (
@@ -20,7 +21,7 @@ export default function CreateUser() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
           <Heading size="lg" fontWeight="normal">
             Criar usuário
           </Heading>
@@ -28,19 +29,25 @@ export default function CreateUser() {
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input name="name" label="Nome completo" />
               <Input name="email" type="email" label="E-mail" />
             </SimpleGrid>
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input name="password" type="password" label="Senha" />
-              <Input name="password_confirfmation" type="password" label="Confirmação da senha" />
+              <Input
+                name="password_confirfmation"
+                type="password"
+                label="Confirmação da senha"
+              />
             </SimpleGrid>
           </VStack>
 
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Link href="/users" passHref>
+                <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
+              </Link>
               <Button colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
